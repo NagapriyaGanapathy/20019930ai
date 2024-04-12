0884622fe69d29862fba2868b43c20c0e2d72c76
Nagapriya Ganapathy <84984674+NagapriyaGanapathy@users.noreply.github.com>
2024-04-12 12:10:18 +0100

    Committing

--- bbcnews.py
+++ bbcnews.py
@@ -3,8 +3,13 @@ from bs4 import BeautifulSoup
 import re
 from tabulate import tabulate
 from pymongo import MongoClient
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 
-def scrape_bbc_news(url):
+def scrape_bbc_news(url): 
   response=requests.get(url)
   if response.status_code==200:
      soup=BeautifulSoup(response.content,'html.parser')
@@ -39,14 +44,23 @@ def preprocess_text(text):
   if text is None:
       return None
 
-  if not text.strip():
-      return None
+  #if not text.strip():
+      #return None
 
-  #text = text.lower()
+  text = text.lower()
 
   text = re.sub(r'http\S+','',text)
   text = re.sub(r'[^a-zA-Z\s]','',text)
   #text = re.sub(r'\s+','',text).strip()
+
+  tokens=word_tokenize(text)
+  
+  tokens=[word for word in tokens if word.isalpha()]
+
+  stop_words=set(stopwords.words('english'))
+  filtered_tokens=[word for word in tokens if word not in stop_words]
+
+  return ' '.join(filtered_tokens)
 
   return text
 
@@ -75,6 +89,10 @@ def fetch_Data(data):
   final_Data = list(table_data.find({},{'_id':0}))
   return final_Data
 def create_and_populate_db():
-  url=""
+  url="https://www.bbc.com/business"
   news_Table= scrape_bbc_news(url)
+  print(hotnews_data,"************")
   insert_Data=(news_Table)
+create_and_populate_db()
+#if__name__=="__main__":
+ 
